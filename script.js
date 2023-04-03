c350c6cee0d80c4e530ba8a911a2019007142dd3
FaithAbiola <abiolafaith92@gmail.com>
2023-04-03 14:55:02 +0100

    image of the day

--- script.js
+++ script.js
@@ -35,5 +35,12 @@ window.onclick = function (event) {
 // menu.classList.toggle("active");
 // });
 
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
 
 
